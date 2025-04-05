0161129916d005c4359f7a8c60c7953428143944
Nagisa <millsmitzi71@gmail.com>
2025-04-05 10:43:29 +0800

    epi1

--- epi1.c
+++ epi1.c
@@ -59,3 +59,34 @@ int read_column_numbers(int columns[], int max)
     while ((ch = getchar()) != EOF && ch != '\n');
     return num;
 }
+
+/* 处理输入行，将指定列的字符连接到一起，输出行以NUL结尾 */
+
+void rearrange(char *output, char const *input, int n_columns, int const columns[])
+{
+    int col; // columns数组的下标
+    int output_col; // 输出列计数器
+    int len; // 输出行的长度
+    
+    len = strlen(input);
+    output_col = 0;
+    
+    // 处理每队列标号
+    for (col = 0; col < n_columns; col += 2)
+    {
+        int nchar = columns[col + 1] - columns[col] + 1;
+        
+        // 如果输入行结束或输出行数组已满，就结束任务。
+        if (columns[col] >= len || output_col = MAX_INPUT - 1)
+            break;
+        
+        // 如果输出行空间不够，只复制可以容纳的数据
+        if (output_col + nchars > MAX_INPUT - 1)
+            nchars = MAX_INPUT - output_col - 1;
+        
+        // 复制相关的数据
+        strncpy(output + output_col,input + columns[col],nchars);
+        output_col += nchars;
+    }
+    output[output_col] = '\0';
+}
