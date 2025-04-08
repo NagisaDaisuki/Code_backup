25277027106e16349f68d4cdabb37a58a4913ee4
Nagisa <millsmitzi71@gmail.com>
2025-04-08 23:22:43 +0800

    update epi5 and readme.md

--- epi3/epi3.c
+++ epi3/epi3.c
@@ -2,6 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 
+/*
+❌ 简短回答：不能直接用 %s 来输出枚举类型的值。
+📌 原因：
+枚举在 C 语言里本质上就是 int 类型的整数；
+
+%s 是格式化字符串输出的，用于 char*（即字符串）；
+
+如果你用 %s 去打印一个枚举值，会导致未定义行为（UB）：可能打印乱码、崩溃甚至无输出。
+*/
 void test1()
 {
     enum Liquid {OUNCE = 1, CUP = 8, PINT = 16, QUART = 32, GALLON = 128};
